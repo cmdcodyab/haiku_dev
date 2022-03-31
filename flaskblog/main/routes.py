003a03af63b1b53e7705bea6a72d0a1ab1fc3065
cmdcodyab <cody212ab@gmail.com>
2022-03-31 17:21:18 +0200

    design avancé

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -16,3 +16,40 @@ def home():
 @main.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@main.route("/consignes")
+def consignes():
+    return render_template('consignes.html', title='About')
+
+
+
+
+@main.route("/haiku de circonstance")
+def circonstance():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('circonstance.html', posts=posts)
+
+
+
+@main.route("/haiku de sagesse")
+def sagesse():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('sagesse.html', posts=posts)
+
+
+
+@main.route("/haiku de saison")
+def saison():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('saison.html', posts=posts)
+
+
+
+@main.route("/senryu")
+def senryu():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('senryu.html', posts=posts)
